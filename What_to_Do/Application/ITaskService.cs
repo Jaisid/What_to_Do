309a477f4d7b23acf8c1c5ac34e9772d05648e6c
Jaiganesh <jaisid4076@gmail.com>
2026-06-18 14:41:21 +0530

    Build MVP task manager with clean architecture

---
+++ What_to_Do/Application/ITaskService.cs
@@ -0,0 +1,11 @@
+﻿using What_to_Do.Domain;
+
+namespace What_to_Do.Application
+{
+    public interface ITaskService
+    {
+        void AddTask(string title);
+        List<Taskitem> GetAll();
+        void CompleteTask(int id);
+    }
+}
---
+++ What_to_Do/Domain/Taskitem.cs
@@ -0,0 +1,9 @@
+﻿namespace What_to_Do.Domain
+{
+    public class Taskitem
+    {    
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public bool IsDone { get; set; }     
+    }
+}
